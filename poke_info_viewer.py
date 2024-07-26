b3b019dbc717527bced569bdef86a7566abb0cc5
pankajkesri <156541463+pankajkesri@users.noreply.github.com>
2024-07-26 16:40:01 -0400

    added required templates

---
+++ poke_info_viewer.py
@@ -0,0 +1,22 @@
+""" 
+Description: 
+  Graphical user interface that displays select information about a 
+  user-specified Pokemon fetched from the PokeAPI 
+
+Usage:
+  python poke_info_viewer.py
+"""
+from tkinter import *
+from tkinter import ttk
+
+# Create the main window
+root = Tk()
+root.title("Pokemon Information")
+
+# TODO: Create the frames
+
+# TODO: Populate the user input frame with widgets
+
+# TODO: Define button click event handler function
+
+root.mainloop()
